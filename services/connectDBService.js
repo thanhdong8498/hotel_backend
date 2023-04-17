4ef458bc44af08657a2ef439c8afb984dc171d00
thanhdong8498 <thanhdonghcmute@gmail.com>
2023-04-18 06:39:57 +0700

    3:55-18/4/2022

--- services/connectDBService.js
+++ services/connectDBService.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDatabase() {
-    const uri = `mongodb+srv://thanhdonghcmute:${process.env.MONGODB_PASSWORD}@cluster0.fs9fxcl.mongodb.net/${process.env.DATABASE_NAME}`;
+    const uri = `mongodb+srv://thanhdonghcmute:thanhdong8498@cluster0.fs9fxcl.mongodb.net/hotel_management`;
     try {
         // await mongoose.connect(`mongodb://127.0.0.1:${process.env.PORT_MONGO}/${process.env.DATABASE_NAME}`);
         await mongoose.connect(uri);
