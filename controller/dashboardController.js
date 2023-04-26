826b25abc2f87639a2facba61949c3b70dee7254
thanhdong8498 <thanhdonghcmute@gmail.com>
2023-04-26 16:14:12 +0700

    finally never change again :v

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -1,6 +1,6 @@
 const bookingModel = require("../models/bookingModel");
 const orderModel = require("../models/orderModel");
-const moment = require("moment");
+
 const getRevenue = async (req, res) => {
     const booking = await bookingModel.find({
         isCheckedOut: true,
@@ -9,16 +9,20 @@ const getRevenue = async (req, res) => {
         isDelivery: true,
     });
     console.log(booking);
-    console.log(moment(new Date()).format("DD/MM/YYYY"));
     const bookingRevenue = booking.map((item) => item.summaryPrice).reduce((prev, current) => prev + current);
     const orderRevenue = order.map((item) => item.totalPrice).reduce((prev, current) => prev + current);
     const totalRevenue = bookingRevenue + orderRevenue;
     const todayOrder = order.filter((item) => {
-        return moment(item.createdAt).format("DD/MM/YYYY") === moment(new Date()).format("DD/MM/YYYY");
+        return (
+            item.updatedAt.toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" }).split(",")[0] ===
+            new Date().toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" }).split(",")[0]
+        );
     });
     const todayBooking = booking.filter((item) => {
-        console.log(item.updatedAt.toString());
-        return moment(item.createdAt).format("DD/MM/YYYY") === moment(new Date()).format("DD/MM/YYYY");
+        return (
+            item.updatedAt.toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" }).split(",")[0] ===
+            new Date().toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" }).split(",")[0]
+        );
     });
     const todayBookingRevenue =
         todayBooking.length > 0
