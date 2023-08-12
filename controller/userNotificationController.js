f03d53c6c7c46d7281abd3af07359e47f37ed6a6
thanhdong8498 <thanhdonghcmute@gmail.com>
2023-08-12 22:32:34 +0700

    change 12/8

--- controller/userNotificationController.js
+++ controller/userNotificationController.js
@@ -1,5 +1,6 @@
+const userModel = require("../models/userModel");
 const userNotificationModel = require("../models/userNotificationModel");
-
+const { getIO } = require("../utils/socket");
 // Get list of all notification belong to userId
 const getListOfNotifications = async (req, res) => {
     const userId = req.userId;
@@ -13,8 +14,50 @@ const getListOfUnreadNotifications = async (req, res) => {
     res.status(200).send(notifications);
 };
 
+const MarkAsRead = async (req, res) => {
+    const notificationId = req.params.id;
+    const notification = await userNotificationModel.findById(notificationId);
+    const userId = notification.userId;
+    const user = await userModel.findById(userId);
+    await userNotificationModel.findByIdAndUpdate(notificationId, { isRead: true }, { new: true });
+    const socketId = user.socketId;
+    const io = getIO(); // Lấy đối tượng io
+    io.to(socketId).emit("notification");
+    res.send("Đã đánh dấu thông báo là đã đọc!");
+};
+
+const deleteNotification = async (req, res) => {
+    const notificationId = req.params.id;
+    const notification = await userNotificationModel.findById(notificationId);
+    const userId = notification.userId;
+    const user = await userModel.findById(userId);
+    const socketId = user.socketId;
+    const io = getIO(); // Lấy đối tượng io
+    io.to(socketId).emit("notification");
+    await userNotificationModel.findByIdAndRemove(notificationId);
+    res.send("Đã xóa thông báo thành công!");
+};
+
+const markAllAsRead = async (req, res) => {
+    const userId = req.userId;
+    console.log(userId);
+    try {
+        const result = await userNotificationModel.updateMany({ userId: userId }, { $set: { isRead: true } });
+        console.log("Marked all notifications as read:", result.nModified, "notifications updated");
+    } catch (error) {
+        console.error("Error marking notifications as read:", error);
+    }
+    const user = await userModel.findById(userId);
+    const socketId = user.socketId;
+    const io = getIO(); // Lấy đối tượng io
+    io.to(socketId).emit("notification");
+    res.send("Đã đánh dấu tất cả là đã đọc! ");
+};
 
 module.exports = {
     getListOfNotifications,
-    getListOfUnreadNotifications
+    getListOfUnreadNotifications,
+    MarkAsRead,
+    deleteNotification,
+    markAllAsRead,
 };
--- router/userNotificationRoute.js
+++ router/userNotificationRoute.js
@@ -5,5 +5,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/list", [authMiddleware.isAuthentication], userNotificationController.getListOfNotifications);
 router.get("/unread", [authMiddleware.isAuthentication], userNotificationController.getListOfUnreadNotifications);
-
+router.put("/mark-as-read/:id", [authMiddleware.isAuthentication], userNotificationController.MarkAsRead);
+router.delete("/delete/:id", [authMiddleware.isAuthentication], userNotificationController.deleteNotification);
+router.put("/mark-all-as-read",[authMiddleware.isAuthentication],userNotificationController.markAllAsRead)
 module.exports = router;
